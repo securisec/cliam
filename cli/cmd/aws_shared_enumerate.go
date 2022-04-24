39de5800387c447c8220e2dccc9fa83c8288d875
securisec <securisec@users.noreply.github.com>
2022-04-23 21:54:26 -0400

    🗓 Apr 23, 2022 9:54:09 PM
    🎨 refator aws cli resource groups as dry

---
+++ cli/cmd/aws_shared_enumerate.go
@@ -0,0 +1,61 @@
+package cmd
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/securisec/cliam/aws/scanner"
+	"github.com/securisec/cliam/aws/signer"
+)
+
+func awsSharedEnumerate(resources []string, saveOutput bool) {
+	key, secret, token, region := getCredsAndRegion()
+	cliLogRegion(awsRegion)
+
+	creds := signer.SetCredentials(key, secret, token, awsProfile)
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	ch := make(chan scanner.ServiceMap, 0)
+	max := make(chan struct{}, MaxThreads)
+
+	go func() {
+		defer wg.Done()
+
+		for s := range ch {
+			wg.Add(1)
+
+			go func(wg *sync.WaitGroup, s scanner.ServiceMap) {
+				max <- struct{}{}
+				ctx, cancel := context.WithTimeout(context.Background(), time.Duration(RequestTimeout)*time.Second)
+
+				defer func() {
+					cancel()
+					<-max
+				}()
+
+				statusCode, err := scanner.EnumerateSpecificResource(ctx, region, s, creds, saveOutput)
+				if err != nil {
+					cliErrorLogger(s, err)
+					wg.Done()
+					return
+				}
+				cliCompletionLogger(s, statusCode)
+
+				wg.Done()
+
+			}(wg, s)
+
+		}
+	}()
+
+	enumerate := scanner.GetServiceMap(resources)
+	for _, e := range enumerate {
+		ch <- e
+	}
+
+	close(ch)
+	wg.Wait()
+}
--- cli/cmd/aws_storage.go
+++ cli/cmd/aws_storage.go
@@ -1,13 +1,7 @@
 package cmd
 
 import (
-	"context"
-	"sync"
-	"time"
-
 	"github.com/securisec/cliam/aws"
-	"github.com/securisec/cliam/aws/scanner"
-	"github.com/securisec/cliam/aws/signer"
 	"github.com/spf13/cobra"
 )
 
@@ -29,47 +23,6 @@ func awsStorageCmdFunc(cmd *cobra.Command, _ []string) {
 
 	saveOutput, _ := cmd.Flags().GetBool("save-output")
 
-	key, secret, token, region := getCredsAndRegion()
-	cliLogRegion(awsRegion)
-
-	creds := signer.SetCredentials(key, secret, token, awsProfile)
-
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-
-	ch := make(chan scanner.ServiceMap, 0)
-	max := make(chan struct{}, MaxThreads)
-
-	go func() {
-		defer wg.Done()
-
-		for s := range ch {
-			wg.Add(1)
-
-			go func(wg *sync.WaitGroup, s scanner.ServiceMap) {
-				max <- struct{}{}
-				ctx, cancel := context.WithTimeout(context.Background(), time.Duration(RequestTimeout)*time.Second)
-
-				defer func() {
-					cancel()
-					<-max
-				}()
-
-				statusCode, err := scanner.EnumerateSpecificResource(ctx, region, s, creds, saveOutput)
-				if err != nil {
-					cliErrorLogger(s, err)
-					wg.Done()
-					return
-				}
-				cliCompletionLogger(s, statusCode)
-
-				wg.Done()
-
-			}(wg, s)
-
-		}
-	}()
-
 	resources := []string{
 		aws.S3,
 		aws.Snowball,
@@ -77,8 +30,5 @@ func awsStorageCmdFunc(cmd *cobra.Command, _ []string) {
 		aws.ElasticFileSystem,
 	}
 
-	awsSendToChannel(ch, resources, awsKnownResourceName)
-
-	close(ch)
-	wg.Wait()
+	awsSharedEnumerate(resources, saveOutput)
 }
